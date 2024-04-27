f211a678bc91eabab24bc11baee54bad66530f6d
TarandeepSingh6267 <164173630+TarandeepSingh6267@users.noreply.github.com>
2024-04-27 16:35:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!,'+name_on_order,icon="✅") 
         # New sectionn to display fruityvice nutrition information
         
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        #st.text(fruityvice_response.json())
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    #st.text(fruityvice_response.json())
+    fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
