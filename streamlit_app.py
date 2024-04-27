fa0696f1b33fc528f2d8cc45de4e742a778544b6
TarandeepSingh6267 <164173630+TarandeepSingh6267@users.noreply.github.com>
2024-04-27 16:33:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ if ingredients_list:
         #st.stop()
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered!,'+name_on_order,icon="✅") 
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!,'+name_on_order,icon="✅") 
         # New sectionn to display fruityvice nutrition information
         
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
