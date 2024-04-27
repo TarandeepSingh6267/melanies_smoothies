e35dc4266bc46c08382f5994db8eab3007f3ff4a
TarandeepSingh6267 <164173630+TarandeepSingh6267@users.noreply.github.com>
2024-04-27 16:32:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ if ingredients_list:
 
         #st.write(my_insert_stmt)
         #st.stop()
-        time_to_insert=st.button('Submit Order')
-        if time_to_insert:
+    time_to_insert=st.button('Submit Order')
+    if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered!,'+name_on_order,icon="✅") 
         # New sectionn to display fruityvice nutrition information
